2a48a1656d75c5caa3c2bc3f90f716b9d1d65ae5
LouKehler <sheldonkehler@gmail.com>
2025-02-27 11:48:21 -0800

    Add gtk.php and language translations

---
+++ gtk.php
@@ -0,0 +1,10 @@
+<?php
+// return Google Translate api - insert the key below
+function gtk($uid) {
+	if ($uid == 'sk') {
+		return '.......................................';
+	} else {
+		return ''; // this will fail intentionally
+	}
+}
+?>
